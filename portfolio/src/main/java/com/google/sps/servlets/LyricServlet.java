a600fb744122f089762afedc9df32d49c7cf1f69
Escanord <anhduy6102@gmail.com>
2022-06-09 15:11:56 +0000

    json communication between client and server
    Signed-off-by: Escanord <anhduy6102@gmail.com>

--- portfolio/src/main/java/com/google/sps/servlets/LyricServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LyricServlet.java
@@ -1,6 +1,10 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +15,18 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/lyric")
 public class LyricServlet extends HttpServlet {
+
+    private static final ArrayList<String> lyrics = new ArrayList<>(List.of("That Arizona sky burnin' in your eyes", "You look at me and, babe, I wanna catch on fire", "It's buried in my soul like California gold", "You found the light in me that I couldn't find"));
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        response.getWriter().print("That Arizona sky burnin' in your eyes \n You look at me and, babe, I wanna catch on fire \n It's buried in my soul like California gold \n You found the light in me that I couldn't find");
+        response.setContentType("application/json");
+        String lyric = convertToJSON(lyrics);
+        response.getWriter().print(lyric);
+    }
+
+    private static final String convertToJSON(List<String> list)
+    {
+        Gson converter = new Gson();
+        return converter.toJson(list);
     }
 }
