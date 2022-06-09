bbae0aee215f75c9098c5413e07f3d0ba5a933c1
Escanord <anhduy6102@gmail.com>
2022-06-09 15:38:17 +0000

    add contact form
    Signed-off-by: Escanord <anhduy6102@gmail.com>

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,18 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String name = request.getParameter("form-name");
+    String email = request.getParameter("form-email");
+    String message = request.getParameter("form-message");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("Hi " + name + "! Thank you for your message as: " + message + "\n and I will get back to you ASAP");
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("Hi " + name + "! Thank you for your message as: " + message + "\n and I will get back to you ASAP");
+    response.getWriter().println(email);
+
+    // Redirect back to main page
+    response.sendRedirect("http://dle-sps-summer22.appspot.com/");
   }
 }
